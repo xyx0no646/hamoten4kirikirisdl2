6efef0bd0024d656cda43c858589fd5d80c4a932
Mikan Puri <xyx0no646@yahoo.co.jp>
2021-02-16 12:47:30 +0900

    change cache strategy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -23,14 +23,48 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener('fetch', event => {
+    if (event.request.url.endsWith('clear')) {
+        // キャッシュをすべて削除する
+        event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName =>caches.delete(cacheName)))));
+        return;
+    }
+    const fetchAndAdd = async request => {
+        const cache = await caches.open(CACHE_VERSION);
+        const response = await fetch(request);
+        cache.put(event.request, response.clone());
+        return response;
+    };
     if (event.request.method != 'GET') return;
     event.respondWith(
         caches.match(event.request)
         .then(res =>{
-            if(res && res.status == 200) 
-                return res;
-             else
-                return fetch(event.request);
+            // キャッシュが存在し、かつ200
+            if (res && res.status == 200) {
+                // オフラインなら必ずキャッシュを戻す
+                if (!navigator.onLine)
+                    return res;
+                // オンラインならheadリクエストを投げて更新を確認
+                else
+                    return fetch(event.request.url,{method:"HEAD"})
+                        .then(headreq =>{
+                            // 更新あり　アクセスする
+                            if (headreq.ok && headreq.status == 200 && 
+                                res.headers.get("ETag") != headreq.headers.get("ETag")) {
+                                return fetchAndAdd(event.request);
+                            }
+                            // 更新なし　キャッシュを戻す
+                            else {
+                                return res;
+                            }
+                        })
+                        //　駄目そうならキャッシュを戻す
+                        .catch(error => {
+                            console.error(error);
+                            return res;
+                        });
+            }
+            else
+                return fetchAndAdd(event.request);
         })
     );
 });
