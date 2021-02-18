b37fe1744d67472e9c490f5500c0248fa02fe1da
xyx0no646_macbook <xyx0no646@yahoo.co.jp>
2021-02-18 21:29:24 +0900

    change webmanifest

--- pwa/swloader.js
+++ pwa/swloader.js
@@ -3,20 +3,9 @@ new class {
         this.initsw();
     }
     async initsw() {
-        // Load WebManifest
-        this.loadManifest("krkrsdl2.appmanifest");
         // Load Service Worker
         const sw = await navigator.serviceWorker.register("./serviceworker.js", {
             scope: "./",
         });
-    }
-    loadManifest(url) {
-        let manifest = document.head.querySelector('#krkrsdl2-web-manifest');
-        manifest.setAttribute('href', url);
-        fetch(url).then(res=>res.json()).then(json=>{
-            if(json.orientation){
-                return ScreenOrientation.lock(json.orientation);
-            }
-        })
     }
 }
